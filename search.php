994457bd1fd28069aafa83b0232a6110a8bf78f2
Xkeeper <xkeeper@gmail.com>
2017-01-30 00:04:20 -0800

    possible start of translation db stuff

--- search.php
+++ search.php
@@ -18,6 +18,8 @@
 	$res		= preg_match_all("#(?:\xa2|\xa9)(..)(?:\x85|\x86)\x05.{0,10}\x22\x3a\x92#s", $rom, $matches, PREG_OFFSET_CAPTURE);
 	$res		= preg_match_all("#(?:\xa2|\xa9)(..)(?:\x85|\x86)\x05.{0,10}\x22(.[\x80-\xFF])\x01#s", $rom, $matches, PREG_OFFSET_CAPTURE);
 
+	$textboxes	= array();
+
 	foreach ($matches[0] as $i => $match) {
 		$dpos	= Utils::toIntLE($matches[1][$i][0]);
 		$jsr	= Utils::toIntLE($matches[2][$i][0]);
@@ -26,8 +28,31 @@
 		$tb		= $itadaki->getTextbox($dposc);
 		$tbta	= explode("\n", (string)$tb);
 
+		$name	= sprintf("textbox_%06x", $dposc);
+		if (!isset($textboxes[$name])) {
+			$textboxes[$name]	= array(
+				'offset'		=> $dpos,
+				'offsetROM'		=> $dposc,
+				'description'	=> $tbta[0],
+			);
+		}
+		$textboxes[$name]['references'][]	= array(
+			'referenceROM'	=> $match[1],
+			'subroutine'	=> $jsr,
+			'code'			=> bin2hex($match[0]),
+			);
 		printf("%06x : %-40s   (%04X, -&gt; %04X)   <a href='test.php?o=0x%05x'>%s</a>\n", $match[1], bin2hex($match[0]), $dpos, $jsr, $dposc, $tbta[0]);
 	}
 
+	print "\n\n";
+
+	foreach ($textboxes as $name => $textbox) {
+		printf("<strong>%s - \$%06x (\$%04x): %s</strong>\n", $name, $textbox['offsetROM'], $textbox['offset'], $textbox['description']);
+		foreach ($textbox['references'] as $ref) {
+			printf("  Referenced at \$%06x, subroutine ptr \$%04x, code %s\n", $ref['referenceROM'], $ref['subroutine'], $ref['code']);
+		}
+	}
+
+	//var_dump($textboxes);
 	print "</pre>";
 	print pageFooter();
