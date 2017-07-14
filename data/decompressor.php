f0c22c466ec088bfe4bd02560137e76b8d10c688
Xkeeper <xkeeper@gmail.com>
2017-07-14 16:27:45 -0700

    Rewrite decompressor; fixes #7 and also makes sense

--- data/decompressor.php
+++ data/decompressor.php
@@ -4,17 +4,16 @@
 
 	class Decompressor {
 
-		protected $_rom				= null;		// The ROM file, as a reference
-		protected $_output			= "";		// The decompressed data
+		protected $_rom					= null;		// The ROM file, as a reference
+		protected $_output				= "";		// The decompressed data
 
 		// Data start and output aren't needed
 		// because they are assumed to both be zero
-		protected $_romOffset		= 0;		// Where in the ROM it starts
-		protected $_readPointer		= 0;		// Where we are in the data
-		protected $_writePointer	= 0;		// Where we are in the output
-		protected $_writeLength		= 0;		// How much we should write
-		protected $_rotatyThing		= 0;		// Unknown state spinner
-		protected $_f2f3			= 0;		// 7E00F2-F3
+		protected $_romOffset			= 0;		// Where in the ROM it starts
+		protected $_readPointer			= 0;		// Where we are in the data
+		protected $_writePointer		= 0;		// Where we are in the output
+		protected $_writeLength			= 0;		// How much we should write
+		protected $_compressionFlag		= 0;		// Compression bitflag
 
 		/**
 		* Constructor, called from ItadakiStreet2->getDecompressor
@@ -26,47 +25,41 @@
 		}
 
 		/**
-		* Decomp_ProbablyStartHere
+		* decompress
+		* Decompress some data based on arguments from the constructor.
 		*/
 		public function decompress() {
-			// All of the various data is already zero
 
-			// Get the amount we're expected to read as a 16-bit value
-			// The original game does two reads/two writes, but we can just do one 16-bit
+			// Get the amount we're expected to read as a 16-bit value.
 			$this->_writeLength		= $this->_readNextByte(true);
 			$this->_log(sprintf("Expected size: %04x bytes", $this->_writeLength));
 
-			// Read two unused (!) bytes. What were these used for??
+			// Read two unused (!) bytes.
+			// The code itself reads and immediately discards these best I can tell
 			$this->_readNextByte();
 			$this->_readNextByte();
 
+			// Continue decompressing data until we fill the requested amount
 			while ($this->_writePointer < $this->_writeLength) {
-				//$this->_log(sprintf("Write pointer: %04x", $this->_writePointer));
-				$this->_log(sprintf("WP %04X - RT %04x", $this->_writePointer, $this->_rotatyThing));
 
-				// If RotaryThing least significant byte is 00, get a new one
-				if (($this->_rotatyThing & 0xFF) == 0x00) {
-					//$this->_log("Getting new rotaty thing");
-					$this->_getNextRotatyThing();
-					//$this->_log(sprintf("  New rotaty thing value: %04x", $this->_rotatyThing));
+				if (($this->_compressionFlag & 0xFF) == 0x00) {
+					// If the low byte of _compressionFlag is 00, get a new one
+					$this->_getNextCompressionFlag();
 
-				} elseif ($this->_shiftRotatyThing()) {
-					// BCS - do something if we shift out a set bit
-					$this->_log("  Doing F2F3 thing");
-					$this->_doF2F3thing();
+				} elseif ($this->_shiftCompressionFlag()) {
+					// If the high bit was set,
+					// the next two bytes are a lookback compression command
+					$this->_log("  Decompress bit set, doing lookback decompression");
+					$this->_doLookbackDecompression();
 
 				} else {
-					// Carry wasn't set, just copy data?
-					// @TODO might need to make a "write to pos" thing
-					// in the event this ends up being able to write arbitrary shit
+					// If the high bit wasn't set, just copy a byte from input to output
 					$byte		= $this->_readNextByte();
 					$this->_log(sprintf("  Copying single byte: %02x", $byte));
 					$this->_output			.= chr($byte);
 					$this->_writePointer++;
 				}
 
-				// wow is that really it? uhhh that f2f3 thing must
-				// really be complicated then huh
 			}
 
 			return $this->_output;
@@ -75,65 +68,81 @@
 
 
 		/**
-		* DoSomethingWithF2F3
+		* _doLookbackDecompression
+		* Implements the lookback decompression format used by the game
+		* Note that this isn't complete lookback, as you can reference "future"
+		* data (in a limited way), and you're limited to about 3 to 19 bytes
+		* (as "compressing" in this way is useless at 2 or under)
+		*
+		* The short version is that the compression is an instruction to
+		* "rewind" the current output by X bytes, and start copying data there
+		* to the end of the output, moving forwards each time
+		*
+		* Assume a lookback of 3 and a length of 5:
+		*                          v current write pointer
+		* Output:   01 02 03 04 05 ..
+		*                 ^ rewind here
+		*
+		* Output:   01 02 03 04 05 03 04 05 03 04 ...
+		*                 ^--------^
+		*                    ^--------^
+		*                       ^--------^
+		*                          ^--------^
+		*                             ^--------^
+		* etc...
 		*/
-		protected function _doF2F3thing() {
+		protected function _doLookbackDecompression() {
 
-			// Two byte value put into F2/F3
-			$this->_f2f3	= $this->_readNextByte(true);
-			$temp			= $this->_f2f3 >> 4;
-			$f4				= $temp;
+			// The game loads two bytes into a 16-bit value...
+			$decompSetting	= $this->_readNextByte(true);
 
-			$this->_log(sprintf("    F2F3 value: %04x", $this->_f2f3));
-			$this->_log(sprintf("    F4 value  : %02x (%d)", $f4, $f4));
-			$this->_log(sprintf("    Write ptr : %04x (%d)", $this->_writePointer, $this->_writePointer));
+			// Then uses the top 12 bits as the offset to look back into the written data, plus 1
+			// and the bottom 4 bits as the length of data to read and write, plus 3
+			$readOfs	= $decompSetting >> 4;
+			$readPtr	= $this->_writePointer - ($readOfs + 1);
+			$readLen	= ($decompSetting & 0x0F) + 3;
 
-			$temp			= $this->_writePointer;
-			$temp--;		// Reduce by one
-			// Subtract temp from F4 value
-			$f4				= $temp - $f4;
+			$this->_log(sprintf("    Original value: %04x", $decompSetting));
+			$this->_log(sprintf("    Lookback: %04x bytes, readPtr %04x", $readOfs, $readPtr));
+			$this->_log(sprintf("    Length:   %02x (%2d)", $readLen, $readLen));
 
-			$this->_log(sprintf("    F4 updated value: %04x (%d)", $f4, $f4));
-
-
-			$temp			= ($this->_f2f3 & 0x0F) + 3;
-
-			$this->_log(sprintf("  Repeat count: %04x (%d)", $temp, $temp));
-
-			for ($i = 0; $i < $temp; $i++) {
-				$byte			= ord($this->_output{$f4});
+			for ($i = 0; $i < $readLen; $i++) {
+				// Read a byte from the output string at position readPtr
+				// and output it at the end of the decompressed output
+				$byte			= ord($this->_output{$readPtr});
 				$this->_log(sprintf("      Copying byte %02x", $byte));
 				$this->_output	.= chr($byte);
 				$this->_writePointer++;
-				$f4++;
+				$readPtr++;
 			}
 		}
 
 
 		/**
-		* GetNextRotatyThing
-		* Loads one byte into A, swaps A/B, loads FF into A
+		* GetNextCompressionFlag
+		* Transfers next byte to high byte of 16-bit value, then adds FF
+		* The bitflag is used to determine if the next bytes are compression data
+		* A high bit shifted out = compressed, otherwise plain copy
 		*/
-		protected function _getNextRotatyThing() {
-			$tmp	= $this->_readNextByte();
-			$this->_rotatyThing	= ($tmp << 8) + 0xFF;
+		protected function _getNextCompressionFlag() {
+			$this->_compressionFlag	= ($this->_readNextByte() << 8) + 0xFF;
 		}
 
 
 		/**
-		* ShiftRotatyThing
+		* ShiftCompressionFlag
 		* This is not an actual subroutine but just a useful helper
-		* @return	if "carry" would be set
+		* @return	top bit shifted out of the 16-bit value
 		*/
-		protected function _shiftRotatyThing() {
+		protected function _shiftCompressionFlag() {
 			// Shift left once
-			$tmp				= ($this->_rotatyThing << 1);
+			$tmp				= ($this->_compressionFlag << 1);
 			// Shift right 16 times to get the carry value
 			$ret				= ($tmp & 0x10000) >> 16;
 
-			//$this->_log(sprintf("  Before %04x - New %04x - Cflag %x", $this->_rotatyThing, $tmp, $ret));
-			// Update rotatything
-			$this->_rotatyThing	= $tmp & 0xFFFF;
+			// Update CompressionFlag
+			$this->_compressionFlag	= $tmp & 0xFFFF;
+
 			// Return shifted-out bit
 			return $ret;
 		}
