728c578c0ac9569a392b2796c66e18e613b21d47
Xkeeper <xkeeper@gmail.com>
2017-07-20 01:45:07 -0700

    organize files a little better

--- includes.php
+++ includes.php
@@ -3,11 +3,11 @@
 	// Require the important stuff
 
 	require "data/translator.php";
-	require "data/itadakistreet2.php";
 	require "data/utils.php";
-	require "data/textbox.php";
-	require "data/decompressor.php";
-	require "data/compressor.php";
+	require "data/itadakistreet2/itadakistreet2.php";
+	require "data/itadakistreet2/textbox.php";
+	require "data/itadakistreet2/decompressor.php";
+	require "data/itadakistreet2/compressor.php";
 	require "html/html.php";
 
 	// Define if we are in CLI mode or not (for some scripts)
