a3d83bd9fb6f9948e5a51dbdd09a90c560b9a356
Xkeeper <xkeeper@gmail.com>
2017-07-22 23:52:20 -0700

    Move big-font to its new home

--- bigtext.php
+++ bigtext.php
@@ -2,7 +2,7 @@
 
 	if (isset($_GET['i'])) {
 		if (substr($_GET['i'], 0, 2) == "0x") $_GET['i'] = hexdec($_GET['i']);
-		$f		= sprintf("big-font/%03x.png", $_GET['i']);
+		$f		= sprintf("data/big-font/%03x.png", $_GET['i']);
 		if (file_exists($f)) {
 			header("Content-type: image/png");
 			readfile($f);
@@ -11,7 +11,8 @@
 	}
 
 	require "includes.php";
-	$itadaki	= new ItadakiStreet2("ita2.sfc", "is2.tbl", null);
+	$itadaki	= getIS2();
+	$rom		= $itadaki->rom();
 
 	if (isset($_GET['o'])) {
 		$o	= hexdec($_GET['o']);
@@ -32,7 +33,7 @@
 				imagecolorallocate($image, 0xE0, 0xE0, 0xE0),
 			);
 
-	$data	= $itadaki->romS($o, 0x12);
+	$data	= substr($rom, $o, 0x12);
 
 	for ($i = 0; $i < (12 * 12); $i++) {
 		$x	= $i % 12;
@@ -42,8 +43,8 @@
 
 	header("Content-type: image/png");
 	imagepng($image);
-
-	imagepng($image, sprintf("big-font/%03x.png", $idx));
+	if (!file_exists("data/big-font/")) mkdir("data/big-font/");
+	imagepng($image, sprintf("data/big-font/%03x.png", $idx));
 
 
 
