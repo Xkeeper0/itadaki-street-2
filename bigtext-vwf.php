e93b2a799fc8f8bf1cba7d016b42de0f6f0f5c6c
Raspberry Floof <raspberry@rosenthalcastle.org>
2018-04-03 13:49:59 -0700

    Extended tester to 4 lines since menu boxes can be 4 lines tall

--- bigtext-vwf.php
+++ bigtext-vwf.php
@@ -17,7 +17,7 @@
 
 	<textarea id="textboxinput" rows="8" cols="40" style="background: #000020; color: #ffffff;"></textarea>
 
-	<canvas id="textboxcanvas" width="128" height="48" style="border:1px solid #d3d3d3; padding: 4px; width: 384px; height: 144px; image-rendering: optimizeSpeed; image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering: -webkit-optimize-contrast; image-rendering: pixelated; image-rendering: optimize-contrast; -ms-interpolation-mode: nearest-neighbor;">Your browser does not support the HTML5 canvas tag.</canvas>
+	<canvas id="textboxcanvas" width="128" height="64" style="border:1px solid #d3d3d3; padding: 4px; width: 384px; height: 192px; image-rendering: optimizeSpeed; image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering: -webkit-optimize-contrast; image-rendering: pixelated; image-rendering: optimize-contrast; -ms-interpolation-mode: nearest-neighbor;">Your browser does not support the HTML5 canvas tag.</canvas>
 
 
 HTML;
